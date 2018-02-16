775bd45983af449e18ea93793cb5fe16975f46f6
Kimple <tingyun.du@gmail.com>
2018-02-16 17:52:55 +0100

    160220184

--- void/views/welcome/index.php
+++ void/views/welcome/index.php
@@ -1,56 +1,4 @@
 
-  <head>
-
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="Intelligent trash sorting system">
-    <meta name="author" content="Tingyun DU">
-
-    <title>Bing Bin | Trions mieux, vivons mieux </title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom fonts for this template -->
-    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Futura:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
-
-
-    <!-- Plugin CSS -->
-    <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="css/creative.min.css" rel="stylesheet">
-    <link href="css/creative.css" rel="stylesheet">
-
-  </head>
-
-  <body id="page-top">
-
-    <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-      <div class="container">
-        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <a class="logo" href="index.html"><img src="img/logo2.png" alt="Logo"></a>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="#about"> À PROPOS</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="#portfolio">REJOINEZ NOUS</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="#contact">CONTACT</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-
 <header class="masthead text-center text-white d-flex">
       <div class="container my-auto">
         <div class="row">
@@ -124,18 +72,13 @@
              <p class="text-black mb-4">
               700€/t pour les biodéchets
               </br>
-              </br>
               500€/t pour le papier et le carton
-              </br>
               </br>
               37€/t pour le verre
               </br>
-              </br>
               1250€/t pour le plastique
               </br>
-              </br>
               1000€/t pour le metal
-              </br>
               </br>
               800€/t pour le textile
              </p>
