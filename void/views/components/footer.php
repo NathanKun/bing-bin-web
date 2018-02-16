daf933a301b755435742ce6dce6eb92438ad59cc
Kimple <tingyun.du@gmail.com>
2018-02-16 18:26:48 +0100

    1602186

---
+++ void/views/components/footer.php
@@ -0,0 +1,8 @@
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-6">
+        Design par <strong><a href="http://www.Themefisher.com">Themefisher</a></strong>
+      </div>
+
+    </div>
+  </div>
--- void/views/components/layout/main.php
+++ void/views/components/layout/main.php
@@ -30,29 +30,7 @@
         ?>
 
     </head>
-    
-    <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-    <div class="container">
-      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <a class="logo" href="<?= site_url(); ?>"><img src="<?= IMG ?>logo2.png" alt="Logo"></a>
-      <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="#about">À PROPOS</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="#portfolio">JOIGNEZ NOUS</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="#contact">CONTACT</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-    </nav>
+
 
     <!-- BODY -->
     <body id="page-top">
