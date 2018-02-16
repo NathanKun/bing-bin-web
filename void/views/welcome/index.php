87c9e2d8e18a8e20482fcf1ee9c1c408b8a216a6
Kimple <tingyun.du@gmail.com>
2018-02-16 16:39:46 +0100

    16022018

--- void/views/welcome/index.php
+++ void/views/welcome/index.php
@@ -6,7 +6,7 @@
           </div>
           <div class="col-lg-8 mx-auto">
             <p class="text-faded mb-5"></p>
-            <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">About Bing Bin</a>
+            <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">À propos Bing Bin</a>
           </div>
         </div>
       </div>
@@ -17,16 +17,15 @@
       <div class="container">
         <div class="row">
           <div class="col-lg-5 text-right">
-            <h2 class="section-heading text-white">It is difficult to sort!</h2>
+            <h2 class="section-heading text-white">Trop de déchets, peu de recyclages.</h2>
             </br>
             <hr class="light1" >
             <p class="text-faded mb-4">
-              Do you know where should we throw a dirty yogurt pot?
-              In France 2016, the cost of wrong sort is 50 millions euro,
-              the cost of non sort is 160 million euro. If everyone sort correctly in France,
-              we will have a gain of 310 million euro. We all know the importance and the advantages of sorting
-              but why is sorting still a global issue? Beause it is difficult to remember all the rules,
-              because we can not see the direct impact of sorting. </p>
+              Chaque année en France, une famille de quatre personnes
+              produit près de 1,75 tonnes de déchets par an, dont plus de 50 %
+               du volume peut être valorisé, soit recyclé, soit transformé
+               en compost ou en gaz de méthanisation. Parmi tous ces
+               déchets seulement 35% sont recyclés. </p>
           </div>
           <div class="col-lg-7 col-sm-3">
             <img class="sticker" src="<?= IMG ?>1x/Fichier 3.png" alt="sticker" >
@@ -42,14 +41,21 @@
                <img class="sticker2" src="<?= IMG ?>1x/Fichier 4.png" alt="sticker2" >
              </div>
              <div class="col-lg-5 text-left">
-               <h2 class="section-heading text-black">Sorting rules are all different!</h2>
+               <h2 class="section-heading text-black">Ne jeter plus votre argent dans la poubelle.</h2>
                </br>
                <hr class="light2">
                <p class="text-black mb-4">
-                 Sorting rules are vary from countries to countries.
-                 In Scandinavian, there are usually seven types of trash.
-                 In France, there are at most 4 types. Sorting rules are not uniform even in one country,
-                 each city has its own rules according to its recyclaling industries.
+                 700€/t pour les biodéchets
+                 </br>
+                 500€/t pour le papier et le carton
+                 </br>
+                 37€/t pour le verre
+                 </br>
+                 1250€/t pour le plastique
+                 </br>
+                 1000€/t pour le metal
+                 </br>
+                 800€/t pour le textile
                </p>
              </div>
            </div>
@@ -66,13 +72,16 @@
        <div class="container">
          <div class="row">
            <div class="col-lg-5 text-right">
-             <h2 class="section-heading text-black">Green trash bin or yellow trash bin?</h2>
+             <h2 class="section-heading text-black">Importance de mieux trier à la source.</h2>
              </br>
              <hr class="light3">
              <p class="text-black mb-4">
-               A survey has been conducted in France 2017, 230 students have responsed.
-               92% of students think sorting is important and more than 50% of them think the disposing system should be improved.
-               The main reason that people don’t sort correctly because they don’t know how to sort.
+               Selon une étude de CITEO, une famille de quatre personnes qui
+                trie ses emballages, permet d’éviter l’émission de 115 kg de
+                 CO2 chaque année, soit l’équivalent de 717 km en voiture et
+                  d’économiser 688 kwh soit l’équivalent de 4590 heures de
+                   télévision. Ce sont par ailleurs, autant de pétrole, sable
+                    ou minerai de fer qui ne seront pas prélevés de nos réserves terrestres.
              </p>
            </div>
            <div class="col-lg-7">
@@ -95,11 +104,11 @@
              <div class="col-lg-12 text-center">
                </br>
                </br>
-               <h3 class="section-heading1 text-white">Bing Bin recognizes your trash and proposes the perfect sort. Events, games, DIY...
-                 Sorting has never been that easy and fun with Bing Bin.</h3>
+               <h3 class="section-heading1 text-white">Bing Bin reconnaît tout type de déchet et vous propose la catégorie de tri appropriée. Events, games, DIY...
+                 Le tri n'a jamais été aussi simple et amusant qu'avec Bing Bin.</h3>
               <hr class="light4">
               </br>
-               <a class="btn btn-light btn-xl js-scroll-trigger" href="#portfolio">Join US!</a>
+               <a class="btn btn-light btn-xl js-scroll-trigger" href="#portfolio">Rejoins-nous !</a>
              </div>
            </div>
          </div>
@@ -120,7 +129,7 @@
 
                   </div>
                   <div class="project-name">
-                    Environment Protection
+                    Protection de l'environnement
                   </div>
                 </div>
               </div>
@@ -135,7 +144,7 @@
 
                   </div>
                   <div class="project-name">
-                    Responsibility
+                    Responsabilité
                   </div>
                 </div>
               </div>
@@ -150,7 +159,7 @@
 
                   </div>
                   <div class="project-name">
-                    Common Interest
+                    Intérêt commun
                   </div>
                 </div>
               </div>
@@ -165,7 +174,7 @@
 
                   </div>
                   <div class="project-name">
-                    Creativity
+                    Creativité
                   </div>
                 </div>
               </div>
@@ -195,7 +204,7 @@
 
                   </div>
                   <div class="project-name">
-                    Do it together
+                    Faisons le ensemble
                   </div>
                 </div>
               </div>
@@ -209,10 +218,10 @@
   <!-- -Get the invite-->
     <section class="bg-dark text-black">
       <div class="container text-center">
-        <h2 class="mb-4 "> Get the invite </h2>
+        <h2 class="mb-4 "> Invitation </h2>
         <hr class="my-4">
         <br />
-        <p> Join Bing Bin family and get access to the app. Stay tuned. </p>
+        <p> Rejoint la famille Bing Bin et soit des premiers à tester notre application ! </p>
         <br />
         <div class="row">
           <div class="col-lg-12" >
@@ -236,9 +245,9 @@
       <div class="container">
         <div class="row">
           <div class="col-lg-8 mx-auto text-center ">
-            <h2 class="section-heading">Let's Get In Touch!</h2>
+            <h2 class="section-heading">Nous sommes la pour vous !</h2>
             <hr class="my-4">
-            <p class="mb-5">Give us a call or send us an email and we will get back to you as soon as possible!</p>
+            <p class="mb-5">Notre équipe est à votre disposition. Contactez-nous !</p>
           </div>
         </div>
         <div class="row">
@@ -256,4 +265,23 @@
       </div>
     </section>
 
+<<<<<<< HEAD
+   <footer class="bg-footer">
+          <div class="container" text-white>
+            <div class="row">
+              <div class="col-sm-8 margin-20" text-white>
+                <ul class="list-inline social">
+                  <li>Rejoins-nous sur    <a href="https://www.facebook.com/bingbinsort/"><i class="fa fa-facebook"></i></li>
+                </ul>
+              </div>
+              <div class="col-sm-4 text-right">
+                <p><small>Copyright &copy; Bing Bin 2018. Tous droits résérvés.
+    	            </small></p>
+              </div>
+            </div>
+          </div>
+        </footer>
+
+=======
+>>>>>>> 778d4224cfb8be1194588dc9c58a0b2c5624982b
 <!-- end of Contact-->
