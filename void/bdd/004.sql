9c23361a36bccfb2520bec00afd7483c0b870309
HE Junyang <nathanhejunyang@gmail.com>
2018-02-22 20:27:09 +0100

    Update script 004, add default value

--- void/bdd/004.sql
+++ void/bdd/004.sql
@@ -3,6 +3,10 @@ alter table Users add id_leaf int;
 
 alter table Users add sun_point int;
 
+ALTER TABLE `Users` CHANGE `eco_point` `eco_point` INT(11) NULL DEFAULT '0';
+ALTER TABLE `Users` CHANGE `sun_point` `sun_point` INT(11) NULL DEFAULT '0';
+ALTER TABLE `Users` CHANGE `id_usagi` `id_usagi` INT(11) NULL DEFAULT '1';
+ALTER TABLE `Users` CHANGE `id_leaf` `id_leaf` INT(11) NULL DEFAULT '1';
 
 create table Sun_Historiques(
     id varchar(50),
